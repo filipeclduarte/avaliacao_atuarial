958544ef4a4a5911eeeab215b040e36c7c11e2e9
Filipe Duarte <filipe_pb_duarte@hotmail.com>
2019-07-30 17:41:27 -0300

    Criando funções

--- avaliacao_atuarial.R
+++ avaliacao_atuarial.R
@@ -26,11 +26,13 @@ tabua <- new("actuarialtable", x = x, lx = lx, interest = 0.06)
 tabua
 
 # Fun??o da probabilidade:
-pxt(tabua, x, t)
+#pxt(tabua, x, t)
 
 # Fun??o anuidade:
-axn(tabua, x, i = 0.06, k = 12, payment = "advance") # antecipada
-axn(tabua, x, i = 0.06, k = 12) # postecipada
+#axn(tabua, x, i = 0.06, k = 12, payment = "advance") # antecipada
+#axn(tabua, x, i = 0.06, k = 12) # postecipada
+
+#axn(tabua, dados$Idade, i = 0.06, k = 12) # postecipada
 
 # Projetar o sal?rio anual
 dados$sal_an <- dados$sal_m*13
@@ -39,29 +41,51 @@ dados
 # Projetar o benef?cio de aposentadoria
 dados$beneficio <- dados$sal_an*(1+0.01)^dados$k  
 dados
-# Aplicar a fun??o da probabilidade 
+# Aplicar a função da probabilidade 
   for(i in 1:length(dados$ID)){
     dados$kpx[i] <- pxt(tabua, dados$Idade[i], t = dados$k[i]) 
     }
 dados
+
+# Outra forma 
+pxt(tabua, dados$Idade, t = dados$k)
 
 # Aplicar a fun??o da anuidade vital?cia
   for(i in 1:length(dados$ID)){
     dados$ar[i] <- axn(tabua, x = dados$r[i], i = 0.06, payment = "advance")
   }
 dados
+# outra forma 
+axn(tabua, x = dados$r, i = 0.06, payment = "advance")
 
 # Calcular o VABF
 dados$vabf <- dados$beneficio*dados$kpx*dados$ar*(1/(1.06)^dados$k)
 dados
+# Outra forma
+VABF <- function(beneficio, kpx, ar, k, i){
+  beneficio*kpx*ar*(1/(1+i)^k)
+}
+VABF(dados$beneficio, dados$kpx, dados$ar, dados$k, i = 0.06)
 
-# Calcular o CN
+# Calcular o CN - Crédito Unitário Projetado
 dados$cn <- dados$vabf/(dados$t+dados$k)
 dados
+# outra forma
+CN <- function(vabf, t, k){
+  vabf/(t+k)
+}
+CN(dados$vabf, dados$t, dados$k)
 
-# Calcular a rm
+# Calcular a RM pelo Crédito Unitário Projetado
 dados$rm <- dados$vabf*(dados$t/(dados$t+dados$k))
 dados
+# outra forma
+RM <- function(vabf, t, k){
+  vabf*(t/(t+k))
+}
+RM(dados$vabf, dados$t, dados$k)
+
+
 
 # Como fa?o para adicionar um novo participante?
 dados[4,] <- c(4, 33, 8, 27, 60, 3200, NA, NA, NA, NA, NA, NA, NA)
@@ -70,21 +94,23 @@ dados
 # Agora tenho que calcular novamente o sal?rio anual, o benef?cio e etc.
 dados$sal_an <- dados$sal_m*13
 dados$beneficio <- dados$sal_an*(1+0.01)^dados$k  
-
-for(i in 1:length(dados$ID)){
-  dados$kpx[i] <- pxt(tabua, dados$Idade[i], t = dados$k[i]) 
-}
-
-
-for(i in 1:length(dados$ID)){
-  dados$ar[i] <- axn(tabua, x = dados$r[i], i = 0.06, payment = "advance")
-}
-
-dados$vabf <- dados$beneficio*dados$kpx*dados$ar*(1/(1.06)^dados$k)
-dados$cn <- dados$vabf/(dados$t+dados$k)
-dados$rm <- dados$vabf*(dados$t/(dados$t+dados$k))
 dados
 
+
+dados$kpx <- pxt(tabua, dados$Idade, t = dados$k)
+dados$ar <- axn(tabua, x = dados$r, i = 0.06, payment = "advance")
+dados$vabf <- VABF(dados$beneficio, dados$kpx, dados$ar, dados$k, i = 0.06)
+dados$cn <- CN(dados$vabf, dados$t, dados$k)
+dados$rm <- RM(dados$vabf, dados$t, dados$k)
+
+dados
+
+# VABF total
+sum(dados$vabf)
+# CN total
+sum(dados$cn)
+# RM total
+sum(dados$rm)
 
 
 
