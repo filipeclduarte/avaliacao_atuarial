ccc89a8d156c933de261cb64d4c824a8c833d621
Filipe Duarte <filipe_pb_duarte@hotmail.com>
2019-07-30 17:41:37 -0300

    Idade de aposentadoria

---
+++ idade_aposentadoria.R
@@ -0,0 +1,23 @@
+# calcular a idade de aposentadoria
+
+idad_apos_2019 <- function(x, sexo, t, profissao){ # sexo 1 == Feminino
+  if(sexo == 1){
+    if(profissao == "PROFESSORA"){
+      return(50)  
+    }else{  
+      return(57)
+    }
+  }else{
+    if(profissao == "PROFESSOR"){
+      return(55)
+    }else{
+      return(62)
+    }
+  }
+}
+
+# testes:
+#idad_apos_2019(sexo = 1, profissao = "PROFESSORA")
+#idad_apos_2019(sexo = 1, profissao = "MÉDICA")
+#idad_apos_2019(sexo = 2, profissao = "PROFESSOR")
+#idad_apos_2019(sexo = 2, profissao = "TÉCNICO")
